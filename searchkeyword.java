e435adefd3b6b7b9e2bc2fe6922f873283d4701e
Aditya Purandare <aditya.p1993@hotmail.com>
2014-08-07 10:34:42 +0530

    Bug Track Project Initialized

---
+++ searchkeyword.java
@@ -0,0 +1,82 @@
+package bugtrack;
+import java.sql.*;
+
+public class searchkeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	
+		String url = "jdbc:mysql://localhost:3306/"; 
+		String dbName = "bugtrack"; 
+		String driver = "com.mysql.jdbc.Driver"; 
+		String userName = "root";
+		String password = ""; 
+		try 
+		{ 
+			Class.forName(driver).newInstance(); 
+			Connection conn = DriverManager.getConnection(url+dbName,userName,password); 
+			Statement st = conn.createStatement(); 
+			ResultSet res = st.executeQuery("SELECT Bug_ID,Summary FROM mozilla_1"); 
+			while(res.next()) 
+			{ 
+				//int bug_id = res.getInt("ID"); 
+				
+				//Captured Summary for a row
+				//Tested on 6th Aug 11:39pm | Aditya | Working
+				String summary = res.getString("Summary"); 
+				//System.out.println(bug_id + "\t" + summary);
+				
+				
+				//Splitting into Words
+				String delims ="[ ,.!()''\"?-]+";
+				String tokens[] = summary.split(delims);
+				
+				//Converting to LowerCase Characters
+				for(int loop_var=0; loop_var<tokens.length; ++loop_var)
+				{	
+					
+					String word = tokens[loop_var].toLowerCase();
+					
+					//Ignoring 2 characters & blank words 
+					//Tested on 6th Aug 11:40pm | Aditya | Working
+					if((word.equals(""))==true || (word.length()<3)) 
+							continue;
+					
+					System.out.println(word+","+tokens.length);
+					
+					Statement st2 = conn.createStatement();
+					ResultSet res2 = st2.executeQuery("SELECT count(*) AS counter FROM mozilla_2 WHERE Word='"+word+"'");
+					//System.out.println(res2);
+					
+					int WordCount=-1;
+					while(res2.next())
+					{
+						WordCount = res2.getInt("counter");
+					}					
+					//System.out.println(WordCount);
+					//Queries re-tested on 6th Aug | 11:43pm | Aditya | working
+					//System.out.println("Word="+word+", Count="+WordCount);
+					if(WordCount==0)
+					{
+						++WordCount;
+						st2.executeUpdate("INSERT into mozilla_2(Word, Count) VALUES('"+word+"', '"+WordCount+"')");
+					}
+					else						
+					{
+						++WordCount;
+						st2.executeUpdate("UPDATE mozilla_2 SET Count='"+WordCount+"' WHERE Word ='"+word+"'");
+					}
+				} 
+			}
+
+			System.out.println("Ran Successfully");
+			conn.close(); 
+		}
+		catch (Exception e) 
+		{ 
+			//e.getMessage();
+			e.printStackTrace(); 
+		} 
+	}
+ } 
